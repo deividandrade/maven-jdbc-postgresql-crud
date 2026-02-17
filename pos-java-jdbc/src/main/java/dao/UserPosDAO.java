32572c521eadd91640340b2770b0de91e0f15fe2
deividandrade <deividandrade.br@gmail.com>
2026-02-17 14:33:46 -0300

    criar consulta SQL ao banco de dados

--- pos-java-jdbc/src/main/java/dao/UserPosDAO.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDAO.java
@@ -2,7 +2,11 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import conexaojdbc.SingleConnection;
 import model.Userposjava;
@@ -35,4 +39,28 @@ public class UserPosDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public List<Userposjava> listar () throws Exception {
+
+	    List<Userposjava> list = new ArrayList<>();
+
+	    String sql = "select * from userposjava";
+
+	    PreparedStatement statement = connection.prepareStatement(sql);
+	    ResultSet resultado = statement.executeQuery();
+
+	    while (resultado.next()) {
+
+	        Userposjava userposjava = new Userposjava();
+	        userposjava.setId(resultado.getLong("id"));
+	        userposjava.setNome(resultado.getString("nome"));
+	        userposjava.setEmail(resultado.getString("email"));
+
+	        list.add(userposjava);
+	    }
+
+	    return list;
+	}
+
+	
 }
